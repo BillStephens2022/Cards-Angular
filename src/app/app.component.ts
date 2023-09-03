9d5e836efdaffc4f8eb255190b943fca76dcb0b2
BillStephens2022 <two4onebill@yahoo.com>
2023-09-03 07:40:05 -0400

    added for loop to generate cards using posts array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
       title: 'Neat Tree',
       imageUrl: 'assets/tree.jpeg',
       username: 'nature',
-      content: 'I saw this tree today'
+      content: 'I saw this tree on my hike today'
     },
     {
       title: 'Snowy Mountain',
